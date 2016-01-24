5b64464d6b72e575864c3cc999d942e912f1aa6b
lyong.liu <lyong.liu@qunar.com>
2016-01-24 15:29:37 +0800

    呵呵

---
+++ tcliuyongTools/JavaThreads/src/main/java/UtilTools/Main.java
@@ -0,0 +1,13 @@
+package UtilTools;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
