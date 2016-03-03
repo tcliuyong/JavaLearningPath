f33c0d237dd1dfaef73e194e9c50a579ba0aa4bf
liuyong <583633876@qq.com>
2016-03-03 16:26:22 +0800

    Fuzzy

---
+++ FuzzyJava/src/main/java/FuzzyJava/dosequal/QuestionOperation.java
@@ -0,0 +1,13 @@
+package FuzzyJava.dosequal;
+
+/**
+ * Created by liuyong on 2016/3/3.
+ */
+public class QuestionOperation {
+    public static void main(String[] args) {
+        char x = 'X';
+        int i = 1;
+        System.out.println(true ? x : 0);
+        System.out.println(false ? i : x);
+    }
+}
---
+++ FuzzyJava/src/main/java/FuzzyJava/lastlaugh/charsecret.java
@@ -0,0 +1,11 @@
+package FuzzyJava.lastlaugh;
+
+/**
+ * Created by liuyong on 2016/3/3.
+ */
+public class charsecret {
+    public static void main(String[] args) {
+        System.out.println("H"+ "a");
+        System.out.println('H' + 'a');
+    }
+}
