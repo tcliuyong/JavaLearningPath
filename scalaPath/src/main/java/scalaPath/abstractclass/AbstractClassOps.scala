fe2004423a8baf546fe6d365580f2936fb57ee71
liuyong <583633876@qq.com>
2016-03-06 10:29:05 +0800

    scala

---
+++ scalaPath/src/main/java/scalaPath/abstractclass/AbstractClassOps.scala
@@ -0,0 +1,29 @@
+package scalaPath.abstractclass
+
+/**
+  * Created by liuyong on 2016/3/6.
+  */
+class AbstractClassOps {
+  var id : Int = _
+}
+abstract class SuperTeacher(val name : String){
+  var id : Int
+  var age : Int
+  def teach
+}
+
+class TeacherForMaths(name : String) extends SuperTeacher(name){
+  override var id = name.hashCode
+  override var age = 20
+  override def teach: Unit ={
+    println("Teaching")
+  }
+}
+object AbstractClassOps{
+  def main(args: Array[String]) {
+    val teacher = new TeacherForMaths("Spark")
+    teacher.teach
+    println(teacher.id)
+    println(teacher.age)
+  }
+}
