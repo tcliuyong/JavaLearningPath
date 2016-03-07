1f37d310b46f02a05d603dbd2ce954b82133c6ef
liuyong <583633876@qq.com>
2016-03-07 21:12:23 +0800

    SpringMVC

---
+++ SpringMVC/src/main/java/SpringMVC/App.java
@@ -0,0 +1,13 @@
+package SpringMVC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
