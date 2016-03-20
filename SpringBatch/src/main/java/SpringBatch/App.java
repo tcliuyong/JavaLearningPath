a175179ad6e85ed04a3d40d334dda6d1d026602f
liuyong <583633876@qq.com>
2016-03-20 09:15:49 +0800

    SpringBatch

--- SpringBatch/src/main/java/SpringBatch/App.java
+++ SpringBatch/src/main/java/SpringBatch/App.java
@@ -1,8 +1,10 @@
 package SpringBatch;
 
+import jdk.nashorn.internal.scripts.JO;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,6 +15,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App 
 {
+    public static void executeJob(String jobPath, String jobName, JobParametersBuilder builder){
+        ApplicationContext context = new ClassPathXmlApplicationContext(jobPath);
+        JobLauncher launcher = (JobLauncher)context.getBean("jobLauncher");
+        Job job = (Job) context.getBean(jobName);
+        try {
+            JobExecution result = launcher.run(job, builder.toJobParameters());
+            System.out.println(result.toString());
+        }catch (Exception e){
+
+        }
+    }
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("base/job.xml");
@@ -23,5 +36,6 @@ public class App
         }catch (Exception e){
             e.printStackTrace();
         }
+        executeJob("base/job.xml","billJob",new JobParametersBuilder().addString("date","21212"));
     }
 }
