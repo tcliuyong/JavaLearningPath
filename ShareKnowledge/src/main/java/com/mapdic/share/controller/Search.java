827c6de9dd9632773297356abca786894fdda29a
liuyong <583633876@qq.com>
2016-04-16 14:48:59 +0800

    ShareKnowledge

--- ShareKnowledge/src/main/java/com/mapdic/share/controller/Search.java
+++ ShareKnowledge/src/main/java/com/mapdic/share/controller/Search.java
@@ -10,6 +10,7 @@ import com.mapdic.share.model.Knowledge;
 import com.mapdic.share.model.Token;
 import com.mapdic.share.serviceimpl.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,8 +37,7 @@ public class Search {
     @Resource
     KnowledgeServiceImpl knowledgeService;
 
-
-    @ResponseBody
+    @ModelAttribute
     @RequestMapping(value ="/search")
     public ModelAndView checkUser(String keyword, int category, int page, int pagesize) {
         ModelAndView modelAndView = new ModelAndView("getKnowledge");
