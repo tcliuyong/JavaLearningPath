749b53d920bc81c9b9bd71bca6efb0b7cf1058ed
liuyong <583633876@qq.com>
2016-05-22 15:41:57 +0800

    SK

--- ShareKnowledge/src/main/resources/backup.sh
+++ ShareKnowledge/src/main/resources/backup.sh
@@ -8,15 +8,15 @@ filebackup="file-"$curtime
 prepicbackup="pic-"$pretime
 prefilebackup="file-"$pretime
 cp -R /home/apache-tomcat-7.0.68/webapps/ROOT/umeditor/jsp/upload /home/backup/$picbackup
-echo $curtime 'picture copy success'>>log
+echo $curtime 'picture copy success'>>/home/backup/log
 cp -R /home/apache-tomcat-7.0.68/webapps/ROOT/uploadFile /home/backup/$filebackup
-echo $curtime 'file copy success'>>log
+echo $curtime 'file copy success'>>/home/backup/log
 if [  -d "/home/backup//$prepicbackup" ]; then
-  echo $curtime 'pic delete success'>>log
+  echo $curtime 'pic delete success'>>/home/backup/log
   rm -R /home/backup/$prepicbackup
 fi
 if [  -d "/home/backup/$prefilebackup" ]; then
   echo "haha"
-  echo $curtime 'file delete success'>>log
+  echo $curtime 'file delete success'>>/home/backup/log
   rm -R /home/backup/$prefilebackup
 fi
