2f9b15660c67749db5bceb0cbb83ef52422493e2
lyong.liu <lyong.liu@qunar.com>
2016-08-10 20:12:54 +0800

    修改评分算法

--- ShareKnowledge/src/main/java/com/mapdic/share/common/LYKWA.java
+++ ShareKnowledge/src/main/java/com/mapdic/share/common/LYKWA.java
@@ -1,9 +1,7 @@
 package com.mapdic.share.common;
 
 import com.mapdic.share.model.Knowledge;
-import com.mapdic.share.serviceimpl.KnowledgeServiceImpl;
 
-import javax.annotation.Resource;
 import java.util.*;
 
 /**
@@ -12,20 +10,20 @@ import java.util.*;
 public class LYKWA {
 
     public  LinkedHashMap sortMap(Map oldMap) {
-        ArrayList<Map.Entry<Knowledge, Integer>> list = new ArrayList<Map.Entry<Knowledge, Integer>>(oldMap.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Knowledge, Integer>>() {
+        ArrayList<Map.Entry<Knowledge, Double>> list = new ArrayList<Map.Entry<Knowledge, Double>>(oldMap.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Knowledge, Double>>() {
 
             @Override
-            public int compare(Map.Entry<Knowledge, Integer> arg0,
-                               Map.Entry<Knowledge, Integer> arg1) {
+            public int compare(Map.Entry<Knowledge, Double> arg0,
+                               Map.Entry<Knowledge, Double> arg1) {
                 if(arg1.getValue() == arg0.getValue())
                    return arg1.getKey().getOrcontent().length() - arg0.getKey().getOrcontent().length()==0?
                            arg1.getKey().getContent().length() - arg0.getKey().getContent().length():
                            arg1.getKey().getOrcontent().length() - arg0.getKey().getOrcontent().length();
-                return arg1.getValue() - arg0.getValue();
+                  return arg1.getValue().compareTo( arg0.getValue());
             }
         });
-        LinkedHashMap<Knowledge, Integer> newMap = new LinkedHashMap();
+        LinkedHashMap<Knowledge, Double> newMap = new LinkedHashMap();
         for (int i = 0; i < list.size(); i++) {
             newMap.put(list.get(i).getKey(), list.get(i).getValue());
         }
@@ -33,9 +31,9 @@ public class LYKWA {
     }
     public LinkedList<Knowledge> lyKeywordAlgorithm(String[] keywords,  List<Knowledge> knowledgeList){
         LinkedList<Knowledge> knowledges = new LinkedList<>();
-        Map<Knowledge,Integer> map = new HashMap<>();
+        Map<Knowledge,Double> map = new HashMap<>();
         for(Knowledge knowledge : knowledgeList){
-            map.put(knowledge,0);
+            map.put(knowledge,0.0);
         }
         for(String str : keywords){
             for(Knowledge knowledge : knowledgeList){
@@ -46,16 +44,25 @@ public class LYKWA {
                 if(knowledge.getTag().toLowerCase().contains(str.toLowerCase())){
                     map.put(knowledge, map.get(knowledge) +  1);
                 }
+
+                if (knowledge.getOrcontent().toLowerCase().contains(str)) {
+                    double num = getKeyWrodsCount(str, knowledge.getOrcontent());
+                    num = num * 0.5;
+                    map.put(knowledge, map.get(knowledge) +  num);
+                }
             }
         }
         Iterator it = sortMap(map).entrySet().iterator();
         while(it.hasNext()){
-            Map.Entry<Knowledge, Integer> entry = (Map.Entry<Knowledge, Integer>) it.next();
+            Map.Entry<Knowledge, Double> entry = (Map.Entry<Knowledge, Double>) it.next();
             //System.out.println(entry.getKey().toString() + " " + entry.getValue());
-            if(entry.getValue() != 0){
+            if(entry.getValue() != 0.0){
                 knowledges.add(entry.getKey());
             }
         }
         return knowledges;
+    }
+    private int getKeyWrodsCount(String keyword, String content) {
+        return content.split(keyword).length;
     }
 }
